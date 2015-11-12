76bdb5f60dcdb506b185d87e0e4ca31d775eebdf
hophacker <jiefeng.hopkins@gmail.com>
2015-11-13 05:31:30 +0800

    .ssh for link.sh

--- link.sh
+++ link.sh
@@ -1,8 +1,9 @@
 #!/bin/bash - 
 set -o nounset                              # Treat unset variables as an error
-rm ~/.bashrc ~/.tmux.conf ~/.vrapperrc ~/.tmuxinator -rf
+rm ~/.bashrc ~/.tmux.conf ~/.vrapperrc ~/.tmuxinator ~/.ssh -rf
 ROOT=`pwd`
 ln $ROOT/.bashrc -s $HOME/.bashrc
 ln $ROOT/.tmux.conf -s $HOME/.tmux.conf
 ln $ROOT/.vrapperrc -s $HOME/.vrapperrc
 ln $ROOT/.tmuxinator -s $HOME/.tmuxinator
+ln $ROOT/.ssh -s $HOME/.ssh
